7aa65311422a49de1a95aa8d76da844a80b5e313
david <dkdavid24@live.com>
2022-05-08 18:16:06 +0300

    add

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def root():
 async def urbanRuralByYear():
     result = []
 
-# urban
+    # urban
     for year, value in data.items():
         urban = value[value['medium'] == "Urban"]
 
@@ -54,6 +54,194 @@ async def urbanRuralByYear():
             }
         )
 
-    print(result)
+    # rural
+    for year, value in data.items():
+        rural = value[value['medium'] == "Rural"]
+
+        avg = np.average(
+                    rural[rural['avg'].notnull()].avg
+                )
+        
+
+        result.append(
+            {
+                "id" : "rural",
+                "x" : year,
+                "y" : round(avg, 2)
+            }
+        )
+
+    return result
+
+
+@app.get("/api/county/{county}")
+async def countyByYear(county):
+    result = []
+
+    for year, value in data.items():
+        tmp = value[value['county']  == county]
+
+        avg = np.average(
+            tmp[tmp['avg'].notnull()].avg
+        )
+
+        result.append(
+            {
+                "id" : county,
+                "x" : year,
+                "y" : round(avg, 2)
+            }
+        )
+
+    return result
+
+@app.get("/api/county/{county}/pass-rate")
+async def countyPassRate(county):
+    result = []
+
+    for year, value in data.items():
+        tmp = value[value['county']  == county]
+        passed_list = tmp[tmp['passed'] == True]
+
+        all = len(tmp)
+
+        passed = len(passed_list)
+
+        result.append(
+            {
+                "id" : county,
+                "x" : year,
+                "y" : round( passed / all, 2)
+            }
+        )
+
+    return result
+
+@app.get("/api/current-previous-gen")
+async def generation():
+    result = []
+
+    # previous generation
+    for year, value in data.items():
+        tmp = value[value['previous_prom'] == True]
+
+        passed_list = tmp[tmp['passed'] == True]
+
+        all = len(tmp)
+
+        passed = len(passed_list)
+
+        result.append(
+            {
+                "id" : "Previous",
+                "x" : year,
+                "y" : round( passed / all, 2)
+            }
+        )
+
+    # current generation
+    for year, value in data.items():
+        tmp = value[value['previous_prom'] == False]
+
+        passed_list = tmp[tmp['passed'] == True]
+
+        all = len(tmp)
+
+        passed = len(passed_list)
+
+        result.append(
+            {
+                "id" : "Current",
+                "x" : year,
+                "y" : round( passed / all, 2)
+            }
+        )
+
+
+    return result
+
+
+@app.get("/api/subject-pass-rate")
+async def subjectPassRate():
+    result = []
+
+    # ro
+    for year, value in data.items():
+        all = value[value['rom_grade'].notnull()]
+        passed = all[all['rom_grade'] >= 5]
+
+        perc = len(passed) / len(all)
+
+        result.append(
+            {
+                "id" : "RO",
+                "x" : year,
+                "y" : round(perc, 2)
+            }
+        )
+
+    # math
+    for year, value in data.items():
+        man = value[value['mandatory'] == "Matematica"]
+
+        all = man[man['mandatory'].notnull()]
+        passed = all[all['mandatory_grade'] >= 5]
+
+        perc = len(passed) / len(all)
+
+        result.append(
+            {
+                "id" : "MAT",
+                "x" : year,
+                "y" : round(perc, 2)
+            }
+        )
+
+    # Ist
+    for year, value in data.items():
+        man = value[value['mandatory'] == "Istorie"]
+
+        all = man[man['mandatory'].notnull()]
+        passed = all[all['mandatory_grade'] >= 5]
+
+        perc = len(passed) / len(all)
+
+        result.append(
+            {
+                "id" : "IST",
+                "x" : year,
+                "y" : round(perc, 2)
+            }
+        )
+
+    # Choice
+    for year, value in data.items():
+        all = value[value['choice_grade'].notnull()]
+        passed = all[all['choice_grade'] >= 5]
+
+        perc = len(passed) / len(all)
+
+        result.append(
+            {
+                "id" : "Choice",
+                "x" : year,
+                "y" : round(perc, 2)
+            }
+        )
+
+    # Native
+    for year, value in data.items():
+        all = value[value['native_grade'].notnull()]
+        passed = all[all['native_grade'] >= 5]
+
+        perc = len(passed) / len(all)
+
+        result.append(
+            {
+                "id" : "Nat",
+                "x" : year,
+                "y" : round(perc, 2)
+            }
+        )
 
     return result
